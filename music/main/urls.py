b9463f3353d04ad6e07630c42ffe7e7c022bc3f1
studsch <schivchyan-ki20@stud.sfu-kras.ru>
2022-11-20 23:16:41 +0700

    add signup

--- music/main/urls.py
+++ music/main/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
+    # path("register/", views.RegisterUser.as_view(), name="register")
+    # path("", include("django.contrib.auth.urls"))
+    path("signup/", views.signup, name="signup"),
 ]
--- music/main/views.py
+++ music/main/views.py
@@ -1,5 +1,36 @@
-from django.http import HttpResponse
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import PlaylistUser
 
 
 def index(request):
     return HttpResponse("It is SurfSound, YO")
+
+
+def signup(request):
+    context= {'username':True,'email':True}
+    if not request.user.is_anonymous:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if (username,) in User.objects.values_list("username",):
+            context['username'] = False
+            return render(request,'signup.html',context)
+
+        elif (email,) in User.objects.values_list("email",):
+            context['email'] = False
+            return render(request,'signup.html',context)
+
+        PlaylistUser.objects.create(username=username)
+        new_user = User.objects.create_user(username, email, password)
+        new_user.save()
+        login(request,new_user)
+        return redirect('/')
+    return render(request,'signup.html',context)
