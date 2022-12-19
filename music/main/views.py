d844c802d8fa34fa53f4423a1d1130ac197151d1
studsch <schivchyan-ki20@stud.sfu-kras.ru>
2022-12-19 10:19:51 +0700

    add player and playlist (liked songs)

--- music/main/views.py
+++ music/main/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from .models import playlist_user
+from youtube_search import YoutubeSearch
 
 
 def index(request):
@@ -60,3 +61,50 @@ def login_auth(request):
 def logout_auth(request):
     logout(request)
     return redirect('/login')
+
+
+def playlist(request):
+    if request.user.is_anonymous:
+        return redirect('/login')
+
+    cur_user = playlist_user.objects.get(username = request.user)
+
+    try:
+      song = request.GET.get('song')
+      song = cur_user.playlist_song_set.get(song_title=song)
+      song.delete()
+    except:
+      pass
+
+    if request.method == 'POST':
+        add_playlist(request)
+        return HttpResponse("")
+    # song = 'kSFJGEHDCrQ'
+    user_playlist = cur_user.playlist_song_set.all()
+    return render(request, 'playlist.html', {'song':song,'user_playlist':user_playlist})
+
+
+def add_playlist(request):
+    cur_user = playlist_user.objects.get(username = request.user)
+
+    if (request.POST['title'],) not in cur_user.playlist_song_set.values_list('song_title', ):
+
+        songdic = (YoutubeSearch(request.POST['title'], max_results=1).to_dict())[0]
+        song__albumsrc=songdic['thumbnails'][0]
+        cur_user.playlist_song_set.create(song_title=request.POST['title'],song_dur=request.POST['duration'],
+        song_albumsrc = song__albumsrc,
+        song_channel=request.POST['channel'], song_date_added=request.POST['date'],song_youtube_id=request.POST['songid'])
+
+
+def search(request):
+    if request.method == 'POST':
+        add_playlist(request)
+        return HttpResponse("")
+    try:
+        search = request.GET.get('search')
+        song = YoutubeSearch(search, max_results=10).to_dict()
+        song_li = [song[:10:2],song[1:10:2]]
+    except:
+        return redirect('/')
+
+    return render(request, 'search.html', {'CONTAINER': song_li, 'song':song_li[0][0]['id']})
